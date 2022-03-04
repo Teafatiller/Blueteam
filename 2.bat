091bebe89b12acb0cdc9ca6c5da07525723dd320
Dakota Teafatiller <56932564+Teafatiller@users.noreply.github.com>
2022-03-03 20:51:07 -0600

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,6 +1,7 @@
-icacls sethc.exe /deny “NT Authority\System”:F
-icacls utilman.exe /deny “NT Authority\System”:F
-icacls osk.exe /deny “NT Authority\System”:F
-icacls magnify.exe /deny “NT Authority\System”:F
-icacls AtBroker.exe /deny “NT Authority\System”:F
-icacls DisplaySwitch.exe /deny “NT Authority\System”:F
+icacls sethc.exe /deny NT Authority\System:F
+icacls utilman.exe /deny NT Authority\System:F
+icacls osk.exe /deny NT Authority\System:F
+icacls magnify.exe /deny NT Authority\System:F
+icacls AtBroker.exe /deny NT Authority\System:F
+icacls DisplaySwitch.exe /deny NT Authority\System:F
+icacls Narrator.exe /deny NT Authority\System:F
